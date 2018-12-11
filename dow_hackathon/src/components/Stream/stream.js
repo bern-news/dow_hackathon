f25635812614fad7cf3048af80b2d823f2f9de0d
Davinder Cheema <davinder.cheema@dowjones.com>
2018-12-11 13:28:42 +0000

    [CHORE] Added Stream component.

--- dow_hackathon/src/components/Stream/stream.js
+++ dow_hackathon/src/components/Stream/stream.js
@@ -5,77 +5,78 @@ require('./style.css');
 
 const Stream = (props) => {
   return (
-    <TextLoop>
+
+      <div>
         <div className="stream stream__container">
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary"> tempor incididunt ut labore et</span>
+                DJ Springboard Presentation
+                <span className="stream--summary"> Ended Nov 20th at 6.00PM GMT</span>
             </div>
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary"> tempor incididunt ut labore et</span>
+                IGNITE Innovation & Insights
+                <span className="stream--summary"> Starting Dec 23rd at 7.00PM GMT</span>
             </div>
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary"> tempor incididunt ut labore et</span>
+                Wellness 360: Presents Patricia Walsh
+                <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
             </div>
         </div>
         <div className="stream stream__container">
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary"> tempor incididunt ut labore et</span>
+                DJ Spotlight: First 100 Days with Josh Stinchcomb
+                <span className="stream--summary"> Starting Dec 15th at 5.00PM GMT</span>
             </div>
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary"> tempor incididunt ut labore et</span>
+                Design Sprint Book Talk
+                <span className="stream--summary"> Starting Dec 15th at 5.00PM GMT</span>
             </div>
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary">tempor incididunt ut labore et</span>
+                DJ Spotlight: The Future Of... Series
+                <span className="stream--summary"> Starting Dec 23rd at 7.00PM GMT</span>
             </div>
         </div>
         <div className="stream stream__container">
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary">tempor incididunt ut labore et</span>
+                Global Wellness Day: Senior Leader Panel Discussion
+                <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
             </div>
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary">tempor incididunt ut labore et</span>
+                Blockchain 101 PT1
+                <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
             </div>
             <div className="stream--item">
                 <span className="stream--image">
-                    <img src="/public/camera.svg" alt="" />
+                    <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                <span className="stream--summary">tempor incididunt ut labore et</span>
+                Blockchain 101 PT2
+                <span className="stream--summary"> Starting Dec 25th at 5.00PM GMT</span>
             </div>
         </div>
-    </TextLoop>
+    </div>
   )
 };
 
