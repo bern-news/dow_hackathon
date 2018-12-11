2551904c67916840b6f952901a706daa4b776b05
Davinder Cheema <davinder.cheema@dowjones.com>
2018-12-11 13:42:08 +0000

    [CHORE] Added Meeting component.

--- dow_hackathon/src/App.js
+++ dow_hackathon/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Calendar from './components/Calendar';
-import Stream from "./components/Stream/stream";
+import Stream from "./components/Stream";
+import Meeting from "./components/Meeting";
 import Legend from './components/Legend';
 import Event from './components/EventFeed';
 import dow_logo from './dow_logo.png';
@@ -24,6 +25,7 @@ class App extends Component {
         </div>
         <div className="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-tablet">
             <Stream/>
+            <Meeting />
         </div>
 
         </div>
