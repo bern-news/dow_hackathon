fc6bfb4d7ef052edeb326cedb46432365e47167c
machelslackatnewsuk <machelsandile.slack@news.co.uk>
2018-12-11 13:16:08 +0000

    event feed wip

--- dow_hackathon/src/App.js
+++ dow_hackathon/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Calendar from './components/Calendar';
+import Event from './components/EventFeed';
 import dow_logo from './dow_logo.png';
 import './App.css';
 import Stream from "./components/Stream/stream";
@@ -11,14 +12,10 @@ class App extends Component {
         <header className="App-header">
           <img src={dow_logo} className="App-logo" alt="logo" />
         </header>
-        <div className="mdl-grid">
-        <div className="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-tablet"> </div>
-        <div className="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-tablet">
-            <Calendar/>
-        </div>
-        <div className="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-tablet">
-            <Stream/>
-        </div>
+        <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-tablet"> <Event /></div>
+        <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-tablet"> <Calendar/> </div>
+        <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-cell--4-col-tablet"> <Stream/></div>
         </div>
         <footer className="App-footer">
         </footer>
---
+++ dow_hackathon/src/components/EventFeed.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './eventfeed.css'
+
+class EventFeed extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="">
+      <div className="headingTable">
+      <h1> FEED OF EVENTS </h1>
+      </div>
+      <div>
+      <ul class="demo-list-two mdl-list">
+  <li class="mdl-list__item mdl-list__item--two-line">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-avatar">person</i>
+      <span>Bryan Cranston</span>
+      <span class="mdl-list__item-sub-title">62 Episodes</span>
+    </span>
+    <span class="mdl-list__item-secondary-content">
+      <span class="mdl-list__item-secondary-info">Actor</span>
+      <a class="mdl-list__item-secondary-action" href="#"><i class="material-icons">star</i></a>
+    </span>
+  </li>
+  <li class="mdl-list__item mdl-list__item--two-line">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-avatar">person</i>
+      <span>Aaron Paul</span>
+      <span class="mdl-list__item-sub-title">62 Episodes</span>
+    </span>
+    <span class="mdl-list__item-secondary-content">
+      <a class="mdl-list__item-secondary-action" href="#"><i class="material-icons">star</i></a>
+    </span>
+  </li>
+  <li class="mdl-list__item mdl-list__item--two-line">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons mdl-list__item-avatar">person</i>
+      <span>Bob Odenkirk</span>
+      <span class="mdl-list__item-sub-title">62 Episodes</span>
+    </span>
+    <span class="mdl-list__item-secondary-content">
+      <a class="mdl-list__item-secondary-action" href="#"><i class="material-icons">star</i></a>
+    </span>
+  </li>
+</ul>
+      </div>
+      </section>);
+  }
+}
+
+export default EventFeed;
