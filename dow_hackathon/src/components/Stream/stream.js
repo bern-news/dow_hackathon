125319ebb8233a0036bc1e9cec357b4ac9ee33c2
Davinder Cheema <davinder.cheema@dowjones.com>
2018-12-11 13:36:55 +0000

    [CHORE] Added Stream component.

--- dow_hackathon/src/components/Stream/stream.js
+++ dow_hackathon/src/components/Stream/stream.js
@@ -1,83 +1,85 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import TextLoop from 'react-text-loop';
 
 require('./style.css');
 
 const Stream = (props) => {
-  return (
-
-      <div>
-        <div className="stream stream__container">
-            <div className="stream--item">
+    return (
+        <Fragment>
+            <h1> DJ Academy </h1>
+            <TextLoop>
+                <div className="stream stream__container">
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                DJ Springboard Presentation
-                <span className="stream--summary"> Ended Nov 20th at 6.00PM GMT</span>
-            </div>
-            <div className="stream--item">
+                        DJ Springboard Presentation
+                        <span className="stream--summary"> Ended Nov 20th at 6.00PM GMT</span>
+                    </div>
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                IGNITE Innovation & Insights
-                <span className="stream--summary"> Starting Dec 23rd at 7.00PM GMT</span>
-            </div>
-            <div className="stream--item">
+                        IGNITE Innovation & Insights
+                        <span className="stream--summary"> Starting Dec 23rd at 7.00PM GMT</span>
+                    </div>
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Wellness 360: Presents Patricia Walsh
-                <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
-            </div>
-        </div>
-        <div className="stream stream__container">
-            <div className="stream--item">
+                        Wellness 360: Presents Patricia Walsh
+                        <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
+                    </div>
+                </div>
+                <div className="stream stream__container">
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                DJ Spotlight: First 100 Days with Josh Stinchcomb
-                <span className="stream--summary"> Starting Dec 15th at 5.00PM GMT</span>
-            </div>
-            <div className="stream--item">
+                        DJ Spotlight: First 100 Days with Josh Stinchcomb
+                        <span className="stream--summary"> Starting Dec 15th at 5.00PM GMT</span>
+                    </div>
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Design Sprint Book Talk
-                <span className="stream--summary"> Starting Dec 15th at 5.00PM GMT</span>
-            </div>
-            <div className="stream--item">
+                        Design Sprint Book Talk
+                        <span className="stream--summary"> Starting Dec 15th at 5.00PM GMT</span>
+                    </div>
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                DJ Spotlight: The Future Of... Series
-                <span className="stream--summary"> Starting Dec 23rd at 7.00PM GMT</span>
-            </div>
-        </div>
-        <div className="stream stream__container">
-            <div className="stream--item">
+                        DJ Spotlight: The Future Of... Series
+                        <span className="stream--summary"> Starting Dec 23rd at 7.00PM GMT</span>
+                    </div>
+                </div>
+                <div className="stream stream__container">
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Global Wellness Day: Senior Leader Panel Discussion
-                <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
-            </div>
-            <div className="stream--item">
+                        Global Wellness Day: Senior Leader Panel Discussion
+                        <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
+                    </div>
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Blockchain 101 PT1
-                <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
-            </div>
-            <div className="stream--item">
+                        Blockchain 101 PT1
+                        <span className="stream--summary"> Starting Dec 24th at 5.00PM GMT</span>
+                    </div>
+                    <div className="stream--item">
                 <span className="stream--image">
                     <img src="/camera.svg" alt="" width="15" height="15"/>
                 </span>
-                Blockchain 101 PT2
-                <span className="stream--summary"> Starting Dec 25th at 5.00PM GMT</span>
-            </div>
-        </div>
-    </div>
-  )
+                        Blockchain 101 PT2
+                        <span className="stream--summary"> Starting Dec 25th at 5.00PM GMT</span>
+                    </div>
+                </div>
+            </TextLoop>
+        </Fragment>
+    )
 };
 
 export default Stream;
