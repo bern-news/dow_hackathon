a06550566cfb866cc9177658eb2d34c97a2de51e
Bern <bernard.o'neill@news.co.uk>
2018-12-11 13:02:05 +0000

    Added calendar legend:

--- dow_hackathon/src/App.js
+++ dow_hackathon/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Calendar from './components/Calendar';
+import Legend from './components/Legend';
 import dow_logo from './dow_logo.png';
 import './App.css';
+import './components/calendar.css'
 
 class App extends Component {
   render() {
@@ -10,7 +12,10 @@ class App extends Component {
         <header className="App-header">
           <img src={dow_logo} className="App-logo" alt="logo" />
         </header>
-        <Calendar/>
+        <div className="CalendarContainer">
+          <Calendar/>
+          <Legend/>
+        </div>
       </div>
     );
   }
