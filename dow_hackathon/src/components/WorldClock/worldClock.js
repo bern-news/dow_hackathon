b5c9c5ee52b37d07d3beeffc070f4574ea86b968
machelslackatnewsuk <machelsandile.slack@news.co.uk>
2018-12-12 10:11:45 +0000

    changed stars to checkboxes adding to do badge

--- dow_hackathon/src/components/WorldClock/worldClock.js
+++ dow_hackathon/src/components/WorldClock/worldClock.js
@@ -6,13 +6,17 @@ class WorldClock extends Component {
 
 	render() {
 		return (
-            <div class="worldClock">
+            <div className="worldClock">
                 <ul>
                     <li><Clock offset='5' name="New York"/></li>
                     <li><Clock offset='0' name="London"/></li>
                     <li><Clock offset='-1' name="Barcelona"/></li>
                     <li><Clock offset='-8' name="Hong Kong"/></li>
+                    <li><span className="mdl-badge badge" data-badge="4" > TO DO</span> </li>
                 </ul>
+
+              
+          
             </div>
 		)
 	}
