55d31dd4e12c2809f576729b6ebef735e7782347
Davinder Cheema <davinder.cheema@dowjones.com>
2018-12-12 11:17:46 +0000

    [CHORE] Fixed scrolling on streamer

--- dow_hackathon/src/components/OmniSearch/omniSearch.js
+++ dow_hackathon/src/components/OmniSearch/omniSearch.js
@@ -1,10 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import './style.css'
 
 class OmniSearch extends Component {
+
+	state = {
+		results: []
+	};
+
+	handleOnChange = () => {
+
+		const { results } = this.state;
+
+		if (results.length === 0) {
+			return this.setState({
+				results: ['Document 1'],
+			})
+		}
+
+		if (results.length === 1) {
+            return this.setState({
+                results: ['Document 1', 'Document 2'],
+            })
+		}
+
+		return this.setState({
+			results: []
+		})
+	};
+
 	render() {
+
+		const {
+			results
+		} = this.state;
+
 		return (
-			<input type="text" name="Search Anywhere" className="omniSearch"/>
+			<Fragment>
+				<input type="text" name="Search Anywhere" className="omniSearch" onChange={this.handleOnChange}/>
+				<div>
+					{results}
+				</div>
+			</Fragment>
+
 		)
 	}
 }
