7eac28b6f0bed46239487c644e3a0f95ab071425
Bern <bernard.o'neill@news.co.uk>
2018-12-12 10:42:12 +0000

    Adding images

--- dow_hackathon/src/components/EventFeed.jsx
+++ dow_hackathon/src/components/EventFeed.jsx
@@ -17,9 +17,10 @@ class EventFeed extends React.Component {
                     <ul class="demo-list-two mdl-list">
                         <li class="mdl-list__item mdl-list__item--two-line">
                             <span class="mdl-list__item-primary-content">
-                                <i class="material-icons mdl-list__item-avatar">person</i>
-                                <span>Bryan Cranston</span>
-                                <span class="mdl-list__item-sub-title">62 Episodes</span>
+                                
+                            <img src="images/course.svg" type="image/svg+xml"/>
+                                <span>Machine Learning for dummies</span>
+                                <span class="mdl-list__item-sub-title">Want to know what machine learning is? Sign up for a beginner masterclass...</span>
                             </span>
                             <span class="mdl-list__item-secondary-content">
                                 <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-1">
@@ -29,9 +30,9 @@ class EventFeed extends React.Component {
                         </li>
                         <li class="mdl-list__item mdl-list__item--two-line">
                             <span class="mdl-list__item-primary-content">
-                                <i class="material-icons mdl-list__item-avatar">person</i>
-                                <span>Aaron Paul</span>
-                                <span class="mdl-list__item-sub-title">62 Episodes</span>
+                            <img src="images/drink.svg" type="image/svg+xml"/>
+                                <span>Christmas drinks</span>
+                                <span class="mdl-list__item-sub-title">London office festive get-together</span>
                             </span>
                             <span class="mdl-list__item-secondary-content">
                             <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-2">
@@ -41,9 +42,9 @@ class EventFeed extends React.Component {
                         </li>
                         <li class="mdl-list__item mdl-list__item--two-line">
                             <span class="mdl-list__item-primary-content">
-                                <i class="material-icons mdl-list__item-avatar">person</i>
-                                <span>Bob Odenkirk</span>
-                                <span class="mdl-list__item-sub-title">62 Episodes</span>
+                            <img src="images/drink.svg" type="image/svg+xml"/>
+                                <span>Times Wine Club</span>
+                                <span class="mdl-list__item-sub-title">Join us on 17 for a special Times wine club tasting event. This will include wines from 10 countires offered at a special discount</span>
                             </span>
                             <span class="mdl-list__item-secondary-content">
                             <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-3">
@@ -51,6 +52,30 @@ class EventFeed extends React.Component {
                                 </label>
                             </span>
                         </li>
+                        <li class="mdl-list__item mdl-list__item--two-line">
+                        <span class="mdl-list__item-primary-content">
+                        <img src="images/christmas.svg" type="image/svg+xml"/>
+                            <span>Christmas day</span>
+                            <span class="mdl-list__item-sub-title">UK / USA- Public holiday</span>
+                        </span>
+                         <span class="mdl-list__item-secondary-content">
+                         <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-3">
+                                    <input type="checkbox" id="list-checkbox-3" class="mdl-checkbox__input"  />
+                                </label>
+                        </span>
+                    </li>
+                    <li class="mdl-list__item mdl-list__item--two-line">
+                        <span class="mdl-list__item-primary-content">
+                        <img src="images/christmas.svg" type="image/svg+xml"/>
+                           <span>Boxing day</span>
+                           <span class="mdl-list__item-sub-title">UK only - Public holiday</span>
+                       </span>
+                        <span class="mdl-list__item-secondary-content">
+                        <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-3">
+                                    <input type="checkbox" id="list-checkbox-3" class="mdl-checkbox__input"  />
+                                </label>
+                       </span>
+                   </li>
                     </ul>
                 </div>
             </section>
